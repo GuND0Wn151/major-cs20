fd161cff0a92f86fd8632f194475144f3d0d3af9
Mahesh <kmaheshgupta151@outlook.com>
2023-03-13 21:21:20 +0530

    Updated Keys

--- backend/utils/Open.js
+++ backend/utils/Open.js
@@ -6,30 +6,12 @@ const stealthPlugin = require("puppeteer-extra-plugin-stealth");
 const { executablePath, default: puppeteer } = require("puppeteer");
 
 puppeteerExtra.use(stealthPlugin());
-Open_api.get("/vmeg", async (req, res) => {
-   const status = await Vmeg();
-   //console.log(status)
-//    let browser = await puppeteerExtra.launch({
-//       headless: false,
-//       args: ["--node-sandbox", "--disable-gpu", "--enable-webgl"],
-
-//       executablePath: executablePath(),
-//    });
-//    console.log("wtfffffffff");
-
-//    page = await browser.newPage();
-//    await page.goto(
-//       "https://studentscorner.vardhaman.org/Students_Corner_Frame.php"
-//    );
-//    //    const cookiesString = await fs.readFile(
-//    //       "C:/Users/ADMIN/Desktop/major/backend/Sessions/vmeg.js"
-//    //    );
-//    //const cookies = JSON.parse(status);
-//    await page.setCookie(...status);
-//    setTimeout(() => {
-//       console.log("...Done!");
-//    }, 10000);
+Open_api.post("/vmeg", async (req, res) => {
+   const data  =req.body
+   console.log(data)
+   const status = await Vmeg(data.username,data.password);
+console.log(status)
    res.send(status);
 });
 
-module.exports = Open_api;
+module.exports = Open_api; 
